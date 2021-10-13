13de34bd61deb29c92fc8a1866bd01c6e9f5cee1
developeranikhossin <mdmanunbiswas321@gmail.com>
2021-10-13 16:04:32 +0600

    monu site

---
+++ js/active.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+// counter 
+
+$('.counter').counterUp({
+delay: 10,
+time: 9000
+});
+
+// animate-active 
+
+new WOW().init();
+
+
+// scrollup 
+
+$.scrollUp({
+    scrollName: 'scrollUp', // Element ID
+    topDistance: '300', // Distance from top before showing element (px)
+    topSpeed: 300, // Speed back to top (ms)
+    animation: 'fade', // Fade, slide, none
+    animationInSpeed: 200, // Animation in speed (ms)
+    animationOutSpeed: 200, // Animation out speed (ms)
+    scrollText: '<i class="fas fa-angle-up"></i>', // Text for element
+    activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+});
+
+});
